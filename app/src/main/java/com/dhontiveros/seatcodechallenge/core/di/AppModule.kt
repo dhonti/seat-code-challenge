0f00b7fa5845dc3d89f585b02f15949f9307a011
David Hontiveros Gordo <davihontigor@gmail.com>
2025-05-24 17:31:24 +0200

    Create corresponding CoroutineDisptachers Qualifiers to be injected through AppModule component

---
+++ app/src/main/java/com/dhontiveros/seatcodechallenge/core/di/AppModule.kt
@@ -0,0 +1,27 @@
+package com.dhontiveros.seatcodechallenge.core.di
+
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.Dispatchers
+
+@Module
+@InstallIn(SingletonComponent::class)
+object AppModule {
+
+    @Provides
+    @IoDispatcher
+    fun provideIoDispatcher(): CoroutineDispatcher = Dispatchers.IO
+
+    @Provides
+    @DefaultDispatcher
+    fun provideDefaultDispatcher(): CoroutineDispatcher = Dispatchers.Default
+
+    @Provides
+    @MainDispatcher
+    fun provideMainDispatcher(): CoroutineDispatcher = Dispatchers.Main
+
+
+}
