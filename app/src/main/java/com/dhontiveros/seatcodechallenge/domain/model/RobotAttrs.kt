26c794c420981164c42af50a82c58c992852c437
David Hontiveros Gordo <davihontigor@gmail.com>
2025-05-24 19:35:16 +0200

    feature/robot-logic-integration: Define algorithm components in order to ease the input process

--- app/src/main/java/com/dhontiveros/seatcodechallenge/domain/model/RobotAttrs.kt
+++ app/src/main/java/com/dhontiveros/seatcodechallenge/domain/model/RobotAttrs.kt
@@ -11,12 +11,48 @@ sealed class Direction {
     data object South : Direction()
     data object East : Direction()
     data object West : Direction()
+
+    fun nextDirection(inputMovement: Movement): Direction =
+        when (inputMovement) {
+            Movement.TurnLeft -> turnLeft()
+            Movement.TurnRight -> turnRight()
+            Movement.MoveForward -> this
+        }
+
+    private fun turnLeft(): Direction = when (this) {
+        North -> West
+        West -> South
+        South -> East
+        East -> North
+    }
+
+    private fun turnRight(): Direction = when (this) {
+        North -> East
+        East -> South
+        South -> West
+        West -> North
+    }
 }
+
 
 data class Position(
     val posX: Long,
     val posY: Long,
     val direction: Direction
 ) {
-    fun nextPos(inputMovement: Movement): Position? = null
+    fun next(inputMovement: Movement): Position =
+        when (inputMovement) {
+            Movement.MoveForward -> when (direction) {
+                Direction.North -> this.copy(posY = posY + 1)
+                Direction.South -> this.copy(posY = posY - 1)
+                Direction.East -> this.copy(posY = posX + 1)
+                Direction.West -> this.copy(posY = posX - 1)
+            }
+
+            Movement.TurnLeft, Movement.TurnRight -> copy(
+                direction = direction.nextDirection(
+                    inputMovement
+                )
+            )
+        }
 }
