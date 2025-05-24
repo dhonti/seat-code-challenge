73727b96fc8951f5669868c4acde3605108675e9
David Hontiveros Gordo <dhontiverosg@axpe.com>
2025-05-24 13:32:57 +0200

    Update MainActivity using some A15 configuration and theme

--- app/src/main/java/com/dhontiveros/seatcodechallenge/presentation/ui/MainActivity.kt
+++ app/src/main/java/com/dhontiveros/seatcodechallenge/presentation/ui/MainActivity.kt
@@ -4,15 +4,20 @@ import android.os.Bundle
 import android.os.PersistableBundle
 import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.material3.MaterialTheme
+import com.dhontiveros.seatcodechallenge.presentation.ui.extensions.configureEdgeToEdgeAppearance
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
+        configureEdgeToEdgeAppearance(isLightMode = true)
         super.onCreate(savedInstanceState, persistentState)
         setContent {
+            MaterialTheme {
 
+            }
         }
     }
 
