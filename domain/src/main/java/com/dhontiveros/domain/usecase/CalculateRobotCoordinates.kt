0b46a3c513ea5c26ab549b4c19217ac5db3f16a0
David Hontiveros <davihontigor@gmail.com>
2025-05-30 09:11:21 +0200

    features/module: Provide correctly the "Default" dispatcher to execute operation in background

--- domain/src/main/java/com/dhontiveros/domain/usecase/CalculateRobotCoordinates.kt
+++ domain/src/main/java/com/dhontiveros/domain/usecase/CalculateRobotCoordinates.kt
@@ -1,6 +1,6 @@
 package com.dhontiveros.domain.usecase
 
-import com.dhontiveros.domain.core.MainDispatcher
+import com.dhontiveros.domain.core.DefaultDispatcher
 import com.dhontiveros.domain.mapper.toDomain
 import com.dhontiveros.domain.model.RobotDomainResult
 import com.dhontiveros.roverrobot.processor.RobotProcessor
@@ -9,12 +9,12 @@ import kotlinx.coroutines.withContext
 import javax.inject.Inject
 
 class CalculateRobotCoordinates @Inject constructor(
-    @MainDispatcher private val mainDispatcher: CoroutineDispatcher,
+    @DefaultDispatcher private val defaultDispatcher: CoroutineDispatcher,
     private val robotProcessor: RobotProcessor
 ) {
 
     suspend operator fun invoke(jsonInput: String): RobotDomainResult =
-        withContext(mainDispatcher) {
+        withContext(defaultDispatcher) {
             robotProcessor.move(jsonInput).toDomain()
         }
 
